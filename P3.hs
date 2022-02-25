0be9cffd881f7466d799e988428de170421194f1
James H. Schader <schadejh@dukes.jmu.edu>
2022-02-25 16:22:13 -0500

    first commit

---
+++ P3.hs
@@ -0,0 +1,41 @@
+--
+-- CS 430 Spring 2019 P3 (Haskell 1)
+--
+-- Name: James Schader
+-- 20220225
+
+module P3 where
+
+-- A list of all factors of n.
+factors :: Integral a => a -> [a]
+factors n = []
+
+-- True iff n is prime.
+isPrime :: Integral a => a -> Bool
+isPrime n = False
+
+-- A list of all prime factors of n.
+primeFactors :: Integral a => a -> [a]
+primeFactors n = []
+
+-- A list of primes up to n.
+primesUpTo :: Integral a => a -> [a]
+primesUpTo n = []
+
+-- True iff n is a perfect number.
+-- A number n is perfect if the sum of its factors is 2*n.
+isPerfect :: Integral a => a -> Bool
+isPerfect n = False
+
+-- A list of all perfect numbers up to n.
+perfectUpTo :: Integral a => a -> [a]
+perfectUpTo n = []
+
+-- The next prime greater than n.
+nextPrime :: Integral a => a -> a
+nextPrime n = n
+
+-- A list of the first n primes.
+generatePrimes :: Integral a => a -> [a]
+generatePrimes n = []
+
